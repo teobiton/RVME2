6a15913ef3fc859bebe8a78698f04a6da09499b6
teobiton <teo.biton@gmail.com>
2023-03-04 14:40:32 +0100

    Describe ADD instruction

--- src/riscvproc.v
+++ src/riscvproc.v
@@ -9,11 +9,11 @@ module system;
 
 // Parameters declaration
 parameter 
-  step = 10,        	 // execution time of one instruction
-  width = 32,       	 // data bus width
-  addrsize = 12,    	 // address bus width
-  memsize = 1<<addrsize, // memory size
-  sbits = 6;        	 // number of status bits
+  step = 10,        	    // execution time of one instruction
+  width = 32,       	    // data bus width
+  addrsize = 12,    	    // address bus width
+  memsize = 1<<addrsize,  // memory size
+  sbits = 6;        	    // number of status bits
 
 // Local parameters declaration
 localparam int unsigned IR_SIZE = 32; // instructions are on 32 bits
@@ -25,13 +25,12 @@ reg[sbits-1:0]    SR;                // status register
 reg[addrsize-1:0] PC;                // program counter
 reg[width-1:0]	  REGS[0:NREGS-1];	 // regfile declaration
 
-//TODO: change instruction fields definition
 /*
 	Instruction Fields
 */
 
 // instruction identifiers
-`define OPCODE  IR[6:0]   		// opcode
+`define OPCODE  IR[6:0]   // opcode
 `define F3 		IR[14:12]		// funct3
 `define F7 		IR[31:25]		// funct7
 
@@ -41,18 +40,18 @@ reg[width-1:0]	  REGS[0:NREGS-1];	 // regfile declaration
 `define RD 		IR[11:7]		// destination register
 
 // immediates
-`define I_IMM 	IR[31:20]					// I-type immediate
+`define I_IMM 	IR[31:20]					          // I-type immediate
 `define S_IMM 	IR[31:25] << 5 || IR[11:7]  // S-type immediate
 
 /*
 	Instruction Declarations
 */
 
-`define ARITHMETIC 	   7'b0110011
-`define IMM_ARITHMETIC 7'b0010011
-`define LOAD 		   7'b0000011
-`define STORE 		   7'b0100011
-`define EBREAK		   7'b1110011
+`define ARITHMETIC 	    7'b0110011
+`define IMM_ARITHMETIC  7'b0010011
+`define LOAD 		        7'b0000011
+`define STORE 		      7'b0100011
+`define EBREAK		      7'b1110011
 
 /* Arithmetic instructions */
 `define ADD 	{3'b000, 7'b0000000}
@@ -108,35 +107,41 @@ end
 	Main cycle for one instruction
 */
 always begin         // execution time of one instruction
+  
   #step
   IR = MEM[PC];      // instruction fetch
   PC = PC + 1;       // increment program counter
   
-  case(`OPCODE)		 // decode instruction
+  case(`OPCODE)		   // decode instruction
   `ARITHMETIC: begin
 	case({`F3, `F7})
-	`ADD:
+	`ADD: begin
+    REGS[`RD] = REGS[`RS1] + REGS[`RS2];
+  end
 	`SUB:
 	`SRL:
 	`SLL:
-	default: $display("erreur: mauvaise valeur dans op-code");
+	default: $display("erreur: mauvaise instruction arithmétique");
 	endcase
   end
   `IMM_ARITHMETIC: begin
 	case(`F3)
 	`ADDI:
 	`ANDI:
-  	endcase
+  default: $display("erreur: mauvaise instruction arithmétique immédiate");
+  endcase
   end
   `LOAD: begin
 	case(`F3)
 	`LB:
-  	endcase
+  default: $display("erreur: mauvaise instruction load");
+  endcase
   end
   `STORE: begin
 	case(`F3)
 	`SB:
-  	endcase
+  default: $display("erreur: mauvaise instruction store");
+  endcase
   end
   `EBREAK: $finish ; // halt
   default: $display("erreur: mauvaise valeur dans op-code");
